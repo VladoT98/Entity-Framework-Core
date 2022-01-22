924e9b30be0209508ee1784e668d49bbc5921aad
Vladislav Tonchev <97668185+VladoT98@users.noreply.github.com>
2022-01-22 15:53:11 +0200

    Delete 1. Exercise ADO.NET directory

--- 1. Exercise ADO.NET/AdoNetExercise/AdoNetExercise/Configuration.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace AdoNetExercise
-{
-    public static class Configuration
-    {
-        public const string CONNECTION_STRING = @"Server=DESKTOP-4HN8DF2\SQLEXPRESS;Database=MinionsDB;Trusted_Connection=True";
-    }
-}
