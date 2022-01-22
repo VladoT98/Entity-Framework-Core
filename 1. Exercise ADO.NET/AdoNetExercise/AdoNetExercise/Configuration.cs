232befc6ac320a3efcb09278698908d6d0ab3b07
Vladislav Tonchev <97668185+VladoT98@users.noreply.github.com>
2022-01-22 15:38:02 +0200

    Add files via upload

---
+++ 1. Exercise ADO.NET/AdoNetExercise/AdoNetExercise/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace AdoNetExercise
+{
+    public static class Configuration
+    {
+        public const string CONNECTION_STRING = @"Server=DESKTOP-4HN8DF2\SQLEXPRESS;Database=MinionsDB;Trusted_Connection=True";
+    }
+}
